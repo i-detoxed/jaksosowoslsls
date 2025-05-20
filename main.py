39d4b0fd68bbe82d6a2cdbdc1d12171de32fa389
i-detoxed <casualstefu@gmail.com>
2025-05-20 12:03:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import threading
 from bookbot import start_bot_logic
+import uvicorn
 
 app = FastAPI()
 
@@ -10,3 +11,6 @@ def home():
 
 # Background me bot logic start karna
 threading.Thread(target=start_bot_logic).start()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
