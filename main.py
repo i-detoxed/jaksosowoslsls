394d76ba70829e6d1ac96d69821f2eaed93fabaf
i-detoxed <casualstefu@gmail.com>
2025-05-20 12:00:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import threading
+from bookbot import start_bot_logic
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "BookBot is running"}
+
+# Background me bot logic start karna
+threading.Thread(target=start_bot_logic).start()
